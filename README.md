8108735558b56420cf89df37bcc741d8d8caef3b
Brandon Horst <brandonhorst@gmail.com>
2016-11-20 17:35:04 -0500

    New website design

--- README.md
+++
@@ -1,2 +0,0 @@
-The Lacona demo website, hosted at www.lacona.io on Github Pages. Any bugs reported will be fixed both on the demo website and in the full version of Lacona.
-
--- front/script.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import Page from './page'
-import isMobile from './detect'
-import {render} from 'react-dom'
-
-render(<Page isMobile={isMobile} />, document.getElementById('page'))
